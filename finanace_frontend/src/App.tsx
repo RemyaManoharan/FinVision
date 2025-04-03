bab515f16de2c0b55b66de21dc78f2c0179e40aa
RemyaManoharan <remyajinil@outlook.com>
2025-04-03 12:46:08 +0200

    added routes to home and features page

--- finanace_frontend/src/App.tsx
+++ finanace_frontend/src/App.tsx
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Layout from "./components/layout/Layout";
+import Home from "./pages/Home";
+import Features from "./pages/FeaturesPage";
 
 function App() {
   return (
@@ -8,7 +10,11 @@ function App() {
       <Router>
         <Routes>
           {/* Public Routes */}
-          <Route path="/" element={<Layout />}></Route>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="/features" element={<Features />} />
+            {/* Add other routes that should include the Layout here */}
+          </Route>
         </Routes>
       </Router>
     </div>
