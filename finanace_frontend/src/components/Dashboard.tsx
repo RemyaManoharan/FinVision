c3dffb27ba07bb3b64c37207cdf0e2969a41c40d
RemyaManoharan <remyajinil@outlook.com>
2025-04-12 10:08:35 +0200

    added sidebar component

--- finanace_frontend/src/components/Dashboard.tsx
+++ finanace_frontend/src/components/Dashboard.tsx
@@ -1,19 +1,20 @@
-import React from 'react'
+import Sidebar from "./Sidebar";
 
 const Dashboard = () => {
   return (
     <div className="flex h-screen">
-      {/* Sidebar - left column */}
+    
       <div className="w-64 border-r border-[rgb(var(--color-border))] bg-[rgb(var(--color-card))]">
         {/* Sidebar content will go here */}
+        <Sidebar />
       </div>
-      
+
       {/* Main content - right column */}
       <div className="flex-1 overflow-auto p-6">
         {/* Dashboard content will go here */}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
