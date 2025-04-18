80112449f94dacb181d1fcf6bfbdd6e0e6af7f4c
RemyaManoharan <remyajinil@outlook.com>
2025-04-18 20:35:49 +0200

    app configured for auth

--- finanace_frontend/src/App.tsx
+++ finanace_frontend/src/App.tsx
@@ -5,24 +5,38 @@ import Features from "./pages/FeaturesPage";
 import SignUpPage from "./pages/SignUpPage";
 import LoginPage from "./pages/LoginPage";
 import Dashboard from "./components/Dashboard";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import AuthProvider from "react-auth-kit/AuthProvider";
+import createStore from "react-auth-kit/createStore";
+
+const store = createStore({
+  authName: "_auth",
+  authType: "cookie",
+  cookieDomain: window.location.hostname,
+  cookieSecure: window.location.protocol === "https:",
+});
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <div>
-      <Router>
-        <Routes>
-          {/* Public Routes */}
-          <Route path="/" element={<Layout />}>
-            <Route index element={<Home />} />
-            <Route path="/features" element={<Features />} />
-            <Route path="/signup" element={<SignUpPage />} />
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/dashboard" element={<Dashboard />} />
-            {/* Add other routes that should include the Layout here */}
-          </Route>
-        </Routes>
-      </Router>
-    </div>
+    <AuthProvider store={store}>
+      <QueryClientProvider client={queryClient}>
+        <Router>
+          <Routes>
+            {/* Public Routes */}
+            <Route path="/" element={<Layout />}>
+              <Route index element={<Home />} />
+              <Route path="/features" element={<Features />} />
+              <Route path="/signup" element={<SignUpPage />} />
+              <Route path="/login" element={<LoginPage />} />
+              <Route path="/dashboard/{user_id}" element={<Dashboard />} />
+              {/* Add other routes that should include the Layout here */}
+            </Route>
+          </Routes>
+        </Router>
+      </QueryClientProvider>
+    </AuthProvider>
   );
 }
 
