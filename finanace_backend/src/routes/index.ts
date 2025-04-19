f277276e78e7c77fd9417b0ce1358d1899e9c26f
RemyaManoharan <remyajinil@outlook.com>
2025-04-19 17:06:54 +0200

    centralised routes in index file

--- finanace_backend/src/routes/index.ts
+++ finanace_backend/src/routes/index.ts
@@ -1,8 +1,13 @@
-import { Router } from 'express';
-import userRoutes from './user.routes';
+import { Router } from "express";
+import userRoutes from "./user.routes";
+import dashboardRoutes from "./dashboard.routes";
 
 const router = Router();
 
-router.use('/users', userRoutes);
+router.use("/users", userRoutes);
+// router.use('/transactions', transactionRoutes);
+// router.use('/assets', assetRoutes);
+// router.use('/budgets', budgetRoutes);
+router.use("/dashboard", dashboardRoutes);
 
 export default router;
